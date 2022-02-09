7c5ff095e69db8ee721b3c062d8e5a17a1a3b529
codedbypolina <98726255+codedbypolina@users.noreply.github.com>
2022-02-08 21:45:47 -0500

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = weekDays[day];
+
+  return `${currentDay} at ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let city = document.querySelector("#city");
@@ -5,12 +24,14 @@ function displayTemperature(response) {
   let description = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#windSpeed");
+  let currentDate = document.querySelector("#date");
 
   city.innerHTML = response.data.name;
   currentTemperature.innerHTML = Math.round(response.data.main.temp);
   description.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = Math.round(response.data.main.humidity);
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  currentDate.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "01e2a719a4f5c5a36214df788b170932";
